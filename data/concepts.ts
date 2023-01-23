ea2e2a6be0c8aef4bf84a6a4317bf0691dc60988
michael@uppergoal.com <michael@uppergoal.com>
2023-01-23 13:09:54 -0500

    Added form, img and script doc

--- data/concepts.ts
+++ data/concepts.ts
@@ -1,11 +1,18 @@
-import { FaCode } from "react-icons/fa";
+import { FaCode, FaLayerGroup } from "react-icons/fa";
 
 export const concepts = [
   {
-    href: "/concepts/global-attributes.mdx",
+    href: "/concepts/global-attributes",
     name: "Global Attributes",
     description:
       "Learn about the group model and how to create, retrieve, update, delete, and list groups.",
     icon: FaCode,
+  },
+  {
+    href: "/concepts/head",
+    name: "The Head",
+    description:
+      "Learn about the head element, how to add metadata to your page, and how to link to external resources.",
+    icon: FaLayerGroup,
   },
 ];
