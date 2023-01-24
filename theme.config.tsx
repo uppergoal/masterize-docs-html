16a3c4aca9fee263f90aca901cb37c3037910783
michael@uppergoal.com <michael@uppergoal.com>
2023-01-24 09:18:31 -0500

    Small fixes

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,12 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>Masterize - HTML Docs</span>,
+  useNextSeoProps: () => {
+    return {
+      title: "Masterize HTML Docs - Masterize",
+      description: "Masterize - HTML Docs",
+    };
+  },
   project: {
     link: "https://github.com/uppergoal/masterize-docs-html",
   },
